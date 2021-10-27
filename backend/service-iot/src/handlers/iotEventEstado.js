a380aae04f73591d6713c7723fd44f45a72440bf
mmondani <mmondani89@gmail.com>
2021-10-26 21:53:10 -0300

    Se empieza a implementar el servicio para parsear los mensajes que vienen por MQTT desde los comunicadores e impactar los cambios en la base de datos

---
+++ backend/service-iot/src/handlers/iotEventEstado.js
@@ -0,0 +1,77 @@
+import { parseHeader, parseRegisterEstado } from "../lib/parser";
+
+const MongoClient = require("mongodb").MongoClient;
+
+let cachedDb = null;
+
+async function connectToDatabase() {
+    if (cachedDb) {
+        return cachedDb;
+    }
+
+    const client = await MongoClient.connect(process.env.MONGODB_CONNECTION_STRING);
+    const db = await client.db(process.env.MONGODB_DB_NAME);
+    
+    cachedDb = db;
+
+    return db;
+}
+
+async function iotEventEstadoSqs(event, context) {
+
+  let records = event.Records;
+
+  const db = await connectToDatabase();
+
+
+  records.forEach(async function (record) {
+    let message = JSON.parse(record.body);
+    let comId = message.clientId;
+    let payload = message.payload;
+    let payloadBuffer = Buffer.from(payload, "hex");
+
+    let parsedMessage = parseHeader(payloadBuffer);
+    let payloadParsed = parseRegisterEstado(parsedMessage);
+
+    console.log (`Mensaje recibido de ${message.clientId} con payload ${message.payload}`);
+    console.log(JSON.stringify(parsedMessage));
+    console.log(JSON.stringify(payloadParsed));
+
+    try {
+      
+      if (parsedMessage.layer === 0) {
+        await db.collection("devices").updateOne(
+          {comId: comId},
+          {$set:{
+            "estadoRedElectrica": payloadParsed.estadoRedElectrica,
+            "estadoBateria": payloadParsed.estadoBateria,
+            "estadoMpxh": payloadParsed.estadoMpxh,
+            "cantidadZonas": payloadParsed.cantidadZonas,
+            "versionFirmware": payloadParsed.versionFirmware
+          }}
+        );
+      }
+
+      await db.collection("devices").updateOne(
+        {comId: comId, "particiones.numero": parsedMessage.layer + 1},
+        {$set:{
+          "particiones.$.estado": payloadParsed.estado,
+          "particiones.$.sonando": payloadParsed.sonando,
+          "particiones.$.ready": payloadParsed.ready,
+          "particiones.$.zonasAnormales": payloadParsed.zonasAnormales,
+          "particiones.$.zonasMemorizadas": payloadParsed.zonasMemorizadas,
+          "particiones.$.zonasIncluidas": payloadParsed.zonasIncluidas,
+          "particiones.$.zonasCondicionales": payloadParsed.zonasCondicionales,
+          "particiones.$.modo": payloadParsed.modo
+        }}
+      );
+    }
+    catch (error) {
+      console.log("[IOT] " + error);
+    }
+  });
+  
+  return 0;
+}
+
+export const handler = iotEventEstadoSqs;
---
+++ backend/service-iot/src/lib/parser.js
@@ -0,0 +1,90 @@
+export const parseHeader = (message) => {
+    let parsedMessage;
+
+    /**
+     * Formato del header
+     *      comando: 1 byte
+     *      registro: 2 bytes
+     *      layer: 1 byte
+     *      fecha: 3 bytes
+     *      hora: 3 bytes
+     *      random: 1 byte
+     *      largo: 2 bytes
+     *      payload: n bytes
+     *      checksum: 1 byte
+     */
+    let sum = 0;
+    for (let i = 0; i < message.length; i ++) {
+        sum += message[i];
+    }
+
+    if (sum & 0xff === 0xff) {
+        // Checksum correcto
+        parsedMessage = {}
+        parsedMessage.comando = message[0];
+        parsedMessage.registro = ((message[2] << 8) & 0xff00) | message[1];
+        parsedMessage.layer = message[3];
+        parsedMessage.timestamp = new Date(message[6] + 2000, message[5], message[4], message[7], message[8], message[9]).getTime();
+        parsedMessage.random = message[10];
+        parsedMessage.largo = ((message[12] << 8) & 0xff) | message[11];
+        parsedMessage.payload = [];
+        for (let i = 0; i < parsedMessage.largo; i++) {
+            parsedMessage.payload.push(message[13 + i]);
+        }
+
+        return parsedMessage;
+    }
+    else {
+        // Error en el checksum
+        return parsedMessage;
+    }
+}
+
+
+let EstadosAlarma = ["null", "desactivada", "activada", "activada_estoy", "activada_me_voy", "activacion_parcial", "programacion"];
+let EstadosBateria = ["bien", "dudosa", "baja"];
+
+export const parseRegisterEstado = (message) => {
+    let payloadParsed = {};
+
+    if (message.comando == 0x0a || message.comando == 0x0b) {
+        payloadParsed.estado = EstadosAlarma[message.payload[0]];
+        payloadParsed.estadoRedElectrica = (message.payload[1] === 0)? false : true;
+        payloadParsed.estadoBateria = EstadosBateria[message.payload[2]];
+        payloadParsed.estadoMpxh = (message.payload[3] === 0)? false : true;
+        payloadParsed.particion = (message.payload[4] === 0)? false : true;
+        payloadParsed.sonando = (message.payload[5] === 0)? false : true;
+        payloadParsed.ready = (message.payload[6] === 0)? false : true;
+        payloadParsed.cantidadZonas = message.payload[7];
+        payloadParsed.versionFirmware = message.payload[8].toString() + "." + message.payload[9].toString();
+
+        payloadParsed.zonasAnormales = 
+            message.payload[10].toString(2).padStart(8, "0") + 
+            message.payload[11].toString(2).padStart(8, "0") + 
+            message.payload[12].toString(2).padStart(8, "0") + 
+            message.payload[13].toString(2).padStart(8, "0");
+        
+
+        payloadParsed.zonasMemorizadas =
+            message.payload[14].toString(2).padStart(8, "0") + 
+            message.payload[15].toString(2).padStart(8, "0") + 
+            message.payload[16].toString(2).padStart(8, "0") + 
+            message.payload[17].toString(2).padStart(8, "0");
+
+        payloadParsed.modo = 'ninguno';
+        if (message.payload[18] != 0)
+            payloadParsed.modo = 'estoy';
+        else if (message.payload[19] != 0)
+            payloadParsed.modo = 'me_voy';
+        
+        payloadParsed.zonasIncluidas = 
+            message.payload[20].toString(2).padStart(8, "0") + 
+            message.payload[21].toString(2).padStart(8, "0") + 
+            message.payload[22].toString(2).padStart(8, "0") + 
+            message.payload[23].toString(2).padStart(8, "0");
+
+        payloadParsed.zonasCondicionales = message.payload[24].toString(2).padStart(4, "0");
+    }
+    
+    return payloadParsed;
+}
